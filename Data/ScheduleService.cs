a18031a22a8227a052bd507c088cb62085a8603b
Frederik Jakobsen <nospam@tez.dk>
2021-04-11 22:44:50 +0200

    Some cleanup + remove publishing info from project file

--- Data/ScheduleService.cs
+++ Data/ScheduleService.cs
@@ -6,21 +6,18 @@ namespace BookingApp.Data
     public class ScheduleService
     {
         public readonly WeeklyTeamSchedule ActiveSchedule =
-            new WeeklyTeamSchedule
+            new()
             {
                 ScheduledTeams = new List<WeeklyScheduledTeam>
                 {
-                    new WeeklyScheduledTeam
+                    new()
                     {
                         Day = DayOfWeek.Monday, TimeOfDay = TimeSpan.FromHours(16) + TimeSpan.FromMinutes(30),
                         TeamId = "1"
                     },
-                    new WeeklyScheduledTeam
-                        {Day = DayOfWeek.Monday, TimeOfDay = TimeSpan.FromHours(18), TeamId = "2"},
-                    new WeeklyScheduledTeam
-                        {Day = DayOfWeek.Saturday, TimeOfDay = TimeSpan.FromHours(18), TeamId = "3"},
-                    new WeeklyScheduledTeam
-                        {Day = DayOfWeek.Saturday, TimeOfDay = TimeSpan.FromHours(17), TeamId = "1"},
+                    new() {Day = DayOfWeek.Monday, TimeOfDay = TimeSpan.FromHours(18), TeamId = "2"},
+                    new() {Day = DayOfWeek.Saturday, TimeOfDay = TimeSpan.FromHours(18), TeamId = "3"},
+                    new() {Day = DayOfWeek.Saturday, TimeOfDay = TimeSpan.FromHours(17), TeamId = "1"},
                 }
             };
     }
